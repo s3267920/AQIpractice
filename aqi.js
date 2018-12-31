8e57f8cedd6286ff11620a707c28b2e9014a4c28
Lucy <s3267920@gmail.com>
2018-12-31 23:06:17 +0800

    bug修正

--- aqi.js
+++ aqi.js
@@ -2,7 +2,7 @@
   // API 來源
   //https://opendata.epa.gov.tw/Data/Contents/AQI/
 
-  let dataExtend = Vue.extend({
+  let cardComponent = {
     props: ['items'],
     computed: {
       status() {
@@ -20,26 +20,18 @@
           return 'status-aqi6';
         }
       }
-    }
-  });
-  let cardComponent = {
-    extends: dataExtend,
-    data() {
-      return {
-        star: false
-      }
     },
     methods: {
-      addStar() {
-        this.star = true;
-        this.$emit('add-stared')
+      starStatus() {
+        this.items.star = !this.items.star;
+        this.$emit('stared-status');
       }
     },
     template: `<div class="card":class="status">
     <div class="card-header">
-      {{items.County}} - {{items.SiteName}} <a href="javascript:;" class="float-right" v-on:click.once="addStar">
-      <div v-show="!star"><i class="far fa-star"></i></div>
-      <div v-show='star'><i class="fas fa-star"></i></div>
+      {{items.County}} - {{items.SiteName}} <a href="javascript:;" class="float-right" v-on:click="starStatus">
+      <div v-show="!items.star"><i class="far fa-star" ></i></div>
+      <div v-show="items.star"><i class="fas fa-star"></i></div>
       </a>
     </div>
     <div class="card-body">
@@ -54,46 +46,15 @@
   </div>`
   };
 
-  let staredComponent = {
-    extends: dataExtend,
-    data() {
-      return {
-        star: false,
-      }
-    },
-    methods: {
-      removeStared() {
-        this.$emit('remove-stared')
-      }
-    },
-    template: `<div class="card":class="status">
-  <div class="card-header">
-    {{items.County}} - {{items.SiteName}} <a href="javascript:;" class="float-right" v-on:click="removeStared">
-    <div ><i class="fas fa-star"></i></div>
-    </a>
-  </div>
-  <div class="card-body">
-    <ul class="list-unstyled">
-      <li>AQI 指數: {{items.AQI}}</li>
-      <li>PM2.5: {{items['PM2.5']}}</li>
-      <li>說明: {{items.Status}}</li>
-    </ul>
-    {{items.PublishTime}}
-  </div>
-</div>
-</div>`
-  };
   let app = new Vue({
     el: '#app',
     data: {
       data: [],
       stared: JSON.parse(localStorage.getItem('staredData')) || [],
-      localStaredData: JSON.parse(localStorage.getItem('staredData')) || [],
       filter: '',
     },
     components: {
       'card-component': cardComponent,
-      'stared-component': staredComponent
     },
     computed: {
       filterCounty() {
@@ -116,24 +77,48 @@
           })
         }
       },
+      getStaredData() {
+        let dataFilter = [];
+        this.stared.forEach(stared => {
+          this.data.forEach(newData => {
+            if (newData.SiteName === stared.SiteName) {
+              this.$set(newData, 'star', true);
+              dataFilter.push(newData);
+            }
+          })
+        })
+        return dataFilter;
+      }
     },
     methods: {
       addToStared(item) {
         //isRepeat 是判斷條件
-        let isRepeat = function (el) {
-          //這邊的el是指將被調用的陣列值
-          return el.SiteName === item.SiteName
-        }
-        let check = this.stared.some(isRepeat);
-        if (!this.stared.length) {
-          this.stared.push(item)
-        } else {
-          if (!check) {
-            this.stared.push(item)
-          } else {
-            return;
+        if (item.star) {
+          let isRepeat = function (el) {
+            //這邊的el是指將被調用的陣列值
+            return el.SiteName === item.SiteName
           }
+          let check = this.stared.some(isRepeat);
+          if (!this.stared.length) {
+            this.stared.push(item);
+
+          } else {
+            if (!check) {
+              this.stared.push(item);
+            } else {
+              return;
+            }
+          }
+        } else {
+          let staredZone = []
+          this.stared.forEach((el, index) => {
+            staredZone.push(el.SiteName);
+            if (staredZone.indexOf(item.SiteName) === index) {
+              this.removeFromStared(index);
+            }
+          })
         }
+        this.setStar();
         this.staredData();
       },
       removeFromStared(index) {
@@ -142,10 +127,23 @@
         this.staredData()
       },
       staredData() {
+        this.setStar();
         let staredDataItem = JSON.stringify(this.stared);
         localStorage.setItem('staredData', staredDataItem);
         let staredData = JSON.parse(localStorage.getItem('staredData'));
         this.stared = staredData;
+      },
+      setStar() {
+        let vm = this;
+        vm.data.forEach(el => {
+          vm.stared.forEach(stared => {
+            if (el.SiteName === stared.SiteName) {
+              vm.$set(el, 'star', true);
+            } else {
+              vm.$set(el, 'star', false);
+            }
+          })
+        })
       }
     },
     mounted() {
@@ -158,8 +156,9 @@
         dataType: 'jsonp'
       }).done(function (response) {
         vm.data = response;
-        console.log(response);
+        // console.log(response);
       });
-    },
+      this.setStar()
+    }
   });
 })();
